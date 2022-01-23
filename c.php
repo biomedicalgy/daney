b1b47a5db8f396e47ebf4c611ae8333208bf00be
biomedicalgy <98257999+biomedicalgy@users.noreply.github.com>
2022-01-23 12:04:42 +0200

    Add files via upload

---
+++ c.php
@@ -0,0 +1,691 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>BME/Mohamed Elgydaney</title>
+
+<link rel="icon" href="logo.jpg">
+</head>
+
+
+<body>
+ 
+<nav id="navb" class="bg-dark">
+ <div class="logo">
+   <img src="logo.jpg">
+ </div>
+      <ul >
+        <li><a href="b.html">Home</a></li>
+        
+        <li><a href="e.html">Electronic Training</a></li>
+        
+        <li><a href="m.html"> BioMedical Training</a></li>
+        <li><a href="x.html">X-RAY</a></li>
+
+        <li><a href="c.html">Conferences</a></li>      
+          <li><a href="https://forms.gle/W76Ccikmqki6uaPCA">Contact</a></li> 
+      </ul>
+
+</nav>
+
+
+
+<!-- showcase -->
+<section id="showcase" class="text-center">
+<div class="showcase-content">
+<h1 class="l-heading    py-1"> LEARN WITH <br> <span class="text-primary">  US  </span> </h1>
+
+<a href="https://forms.gle/W76Ccikmqki6uaPCA" class="btn" >Become A Member</a>
+
+</div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- about -->
+<section  id="about" class="bg-light" >   
+<div class="container">
+<h2 class="m-heading     text-center py-1">Hello we are  <span class="text-primary">  BioMdical Training  </span></h2>
+<div class="about-content">
+<div>
+<p> In the 1950s, we saw innovation in technology across all fields, but most importantly 
+    there were many breakthroughs which led to modern medicine. On 6 March 1953, Dr. Jonas Salk
+     announced the completion of the first successful killed-virus Polio vaccine. The vaccine was 
+     tested on about 1.6 million Canadian, American, 
+    and Finnish children in 1954. The vaccine was announced as safe on 12 April 1955
+<br><br><p>
+ 
+</p>
+</div>
+<img src="m5.jpg" class="box-shadow">
+</div>
+
+
+
+</div>
+
+
+</section>
+
+
+
+
+
+<!-- about -->
+<section  id="about" class="bg-light" >   
+    <div class="container">
+    <h2 class="m-heading     text-center py-1">  </span></h2>
+    <div class="about-content">
+    <div>
+    <p>  
+        During and after World War II, the field of biomedical science saw a new age of
+         technology and treatment methods. For instance in 1941 the first hormonal treatment for 
+         prostate cancer was implemented by Urologist and cancer researcher Charles B. Huggins. Huggins 
+         discovered that if you remove the testicles from a man with prostate cancer, 
+        the cancer had nowhere to spread, and nothing to feed on thus putting the subject into remission
+
+ 
+    <br><br><p>
+     
+    </p>
+    </div>
+    <img src="m4.jpg" class="box-shadow">
+    </div>
+    
+    
+    
+    </div>
+    
+    
+    </section>
+    
+    
+    
+
+    
+
+
+
+    
+
+<!-- about -->
+<section  id="about" class="bg-light" >   
+    <div class="container">
+    <h2 class="m-heading     text-center py-1">  </span></h2>
+    <div class="about-content">
+    <div>
+    <p> 
+
+
+
+
+        This advancement lead to the development of hormonal blocking drugs, which is less invasive and still used today.
+         At the tail end of this decade, the first bone marrow transplant was done on a mouse in 1949. The surgery was conducted by Dr.
+          Leon O. Jacobson, he discovered that he could transplant bone
+         marrow and spleen tissues in a mouse that had both no bone marrow and a destroyed spleen
+
+
+    </p>
+    </div>
+    <img src="m3.jpg" class="box-shadow">
+    </div>
+    
+    
+    
+    </div>
+    
+    
+    </section>
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+  
+<center>
+
+
+ 
+<section id="show" class="text-center">
+<div class="showcase-content">
+<h1 class="l-heading    py-1"> Do not Think <br>
+Learn  <span class="text-primary">  With Us</span> </h1>   
+
+
+
+ <div class="about-content">
+<div>
+<p>
+    Biomedical Science in the 20th century
+    At this point in history the field of medicine was the most
+     prevalent sub field of biomedical science, as several breakthroughs on
+     how to treat diseases and help the immune system were made. As well as the birth of body augmentations.</p>
+
+<br><br></p>
+</div>
+
+
+
+
+
+
+
+</div>
+</div></section>
+
+
+
+
+
+
+
+</center> 
+
+
+
+
+ 
+
+
+
+
+
+</body>
+
+<style>
+
+
+
+
+.m{
+ color: goldenrod;
+}
+
+
+h4,p{
+ margin: 15px;
+}
+
+.box img{
+ width: 50vw;
+ background-color: aqua;
+ padding: 8px;
+ border-radius: 100%;
+}
+.trainers-boxes{
+ display: flex;
+
+}
+
+.trainer-box{
+ background-color: white;
+ flex: 1;
+ border-radius: 5px;
+ margin: 16px;
+}
+.trainer-box img{
+display: block;
+width: 40%;
+float: left;
+margin-bottom: 10px;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      /* reset */
+*{
+ margin: 0;
+ padding: 0;
+ box-sizing: border-box;
+}
+    /* main */
+
+a{
+ text-decoration: none;
+}
+body{
+line-height: 1.5;
+}
+.container{
+ max-width: 70%;
+ margin: auto;
+
+}
+html{
+ background-color: lightgray;
+}
+.bg-dark{
+ background-color: #252a34;
+ color: white;
+}
+#navb
+{
+
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+padding: 16px;
+position: sticky;
+top: 0;
+z-index: 1;
+}
+.logo img{
+ width: 80px;
+}
+a{
+ padding: 16px;
+
+ color: white;
+
+}
+ul{
+ list-style: none;
+display: flex;
+
+
+
+}
+a:hover{
+ background-color: aqua;
+ border-radius: 15px;
+
+}
+img{
+ border: 2px solid gray;
+}
+.text-primary{
+ color: goldenrod;
+
+}
+.text-center{
+ text-align: center;
+}
+.l-heading{
+ font-size: 45px;
+ text-transform: uppercase;
+}
+.py-1{
+ padding: 20px 0;
+}
+.btn{
+ background-color: aqua;
+color: white;
+padding: 8px 20px;
+border-radius: 20px;
+
+}
+
+
+.btn:hover{
+ background-color: goldenrod;
+}
+
+#showcase{
+ background:   aqua url(h3.jpg) no-repeat center center/cover  ;
+background-repeat: no-repeat;
+ height: 100vh;
+ 
+
+color: white;
+}
+.showcase-content{
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ justify-content: center;
+ height: 100%;
+ padding: 16px;
+ background-color: rgba(0, 0, 0, 0.7);
+}
+
+
+#show{
+ background:   aqua url(h1.jpg) no-repeat center center/cover  ;
+background-repeat: no-repeat;
+ height: 100vh;
+ 
+
+color: white;
+}
+
+
+
+
+
+
+.bg-light{
+ background-color:lightgray;
+ color:#252a34; 
+}
+
+.box-shadow{
+
+box-shadow: 0 0 2px silver;}
+
+
+
+.about-content{
+display: flex;
+align-items: center;
+justify-content: center;
+padding-bottom:20px ;
+
+}
+
+.about-content div ,.about-content img{
+ flex: 1;
+}
+.about-content img{
+ width: 35vw;
+ height: 40vh;
+margin-left: 40px;
+border: 4px solid gray;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</style>
+<script>
+
+</script>
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
